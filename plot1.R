e22df6c11a2869a30cf15579c784ae5e3fba78b4
abhijitb <abhijit.bhinge@gmail.com>
2015-04-11 17:17:30 -0500

    R file to generate graph

---
+++ plot1.R
@@ -0,0 +1,31 @@
+## plot1.R
+library(sqldf)
+
+## Open a file connection
+## The household_power_consumption.txt has to be in the same folder as this R file.
+fileConn <- file("household_power_consumption.txt") 
+
+
+## select necessary data instead of reading the entire file.
+df <- sqldf("select * from fileConn where (Date = '1/2/2007' or Date = '2/2/2007')", file.format = list(header = TRUE, sep = ";"))
+
+## Close the file connection
+close(fileConn)
+
+## convert the Date and Time columns Date/Time classes.
+df$Date <- as.Date(sqldf$Date, "%d/%m/%Y")
+
+datetime <- paste(df$Date, df$Time)
+
+## add a new column called DateTime
+df$DateTime <- as.POSIXct(datetime)
+
+## open the png device
+png(filename = "plot1.png")
+
+## plot histogram
+hist(df$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
+
+## close the device
+dev.off()
+
