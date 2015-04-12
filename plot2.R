7dfd1c1d113d6edf2cd7f797eb04200e0277e9d8
abhijitb <abhijit.bhinge@gmail.com>
2015-04-12 08:12:17 -0500

    R file to generate graph

--- plot2.R
+++ plot2.R
@@ -13,7 +13,7 @@ df <- sqldf("select * from fileConn where (Date = '1/2/2007' or Date = '2/2/2007
 close(fileConn)
 
 ## convert the Date and Time columns Date/Time classes.
-df$Date <- as.Date(sqldf$Date, "%d/%m/%Y")
+df$Date <- as.Date(df$Date, "%d/%m/%Y")
 
 datetime <- paste(df$Date, df$Time)
 
@@ -21,7 +21,7 @@ datetime <- paste(df$Date, df$Time)
 df$DateTime <- as.POSIXct(datetime)
 
 ## open the png device
-png(filename = "plot2.png")
+png(filename = "plot2.png", width = 480, height = 480, units = "px") 
 
 ## plot line graph
 
